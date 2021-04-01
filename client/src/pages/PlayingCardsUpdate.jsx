1301b38d66bf94836202d215896a221fd39e06c9
Phil Brockman <phil.brockman@gmail.com>
2021-04-01 14:12:56 -0400

    connected to MTG API!

--- client/src/pages/PlayingCardsUpdate.jsx
+++ client/src/pages/PlayingCardsUpdate.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 
-class PlayingCardsUpdate extends Component {
-    render() {
+import * as Magic from "mtgsdk";
+
+// Magic.card.find(3)
+// .then(result => {
+//     console.log(result.card.name) // "Black Lotus"
+// })
+
+const PlayingCardsUpdate = props => {
+
         return (
             <div>
+                <p>{JSON.stringify(props)}</p>
                 <p>In this page you'll see the form to update the movies</p>
             </div>
         )
-    }
+
 }
 
 export default PlayingCardsUpdate
