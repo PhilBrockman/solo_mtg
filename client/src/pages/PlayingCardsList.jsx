04ed025e532d58fd798d755da3992d3ebbc17bbe
Phil Brockman <phil.brockman@gmail.com>
2021-03-31 22:46:47 -0400

    Update PlayingCardsList.jsx
    minimal functional version

--- client/src/pages/PlayingCardsList.jsx
+++ client/src/pages/PlayingCardsList.jsx
@@ -1,10 +1,9 @@
 
-import React, { Component, useEffect } from 'react'
-import ReactTable from 'react-table'
+import React from 'react'
+import { useTable } from 'react-table'
+
 import api from '../api'
 
-console.log("aeou")
-console.log("getting all cards", api.getPlayingCardById)
 // import styled from 'styled-components'
 
 // import 'react-table/react-table.css'
@@ -15,76 +14,51 @@ console.log("getting all cards", api.getPlayingCardById)
 
 // const Wrapper = () => {<div></div>}
 
+
+
+
 const PlayingCardsList = () => {
-
-    const [loaded, setLoaded] = React.useState(false)
-    const [playingCards, setPlayingCards] = React.useState([])
-
-    console.log('hello?')
+    const [playingCards, setPlayingCards] = React.useState(null)
 
     React.useEffect(() => {
         console.log("running effect")
         api.getAllPlayingCards().then(playingCards => {
             console.log("in effect", playingCards.data.data)
-            setLoaded(false)
             setPlayingCards(playingCards)
         })
        }, []);
 
-
     console.log('TCL: PlayingCardsList -> render -> playingCards', playingCards)
 
-    const columns = [
-        {
-            Header: 'ID',
-            accessor: '_id',
-            filterable: true,
-        },
-        {
-            Header: 'Name',
-            accessor: 'name',
-            filterable: true,
-        },
-        {
-            Header: 'Rules Text',
-            accessor: 'rulesText',
-            filterable: true,
-        },
-        {
-            Header: 'Token',
-            accessor: 'token',
-            filterable: true,
-        },
-    ]
-
-    let showTable = true
-        if (!playingCards.length) {
-            showTable = false
-        }
 
     return (
-        <>
-            {showTable && (
-                <ReactTable
-                    data={playingCards}
-                    columns={columns}
-                    loading={!loaded}
-                    defaultPageSize={10}
-                    showPageSizeOptions={true}
-                    minRows={0}
-                />
-            )}
-
-            {!showTable && (
-                <>
-                    {JSON.stringify(playingCards)}
-                </>
-            )}
-        </>
+     <>
+        <MTGTable 
+            data={playingCards}
+            />
+    </>
     )
 
-        
+    // return (
+    //     <>
+    //         {showTable && (
+                
+    //         )}
+
+    //         {!showTable && (
+    //             <>
+    //                 {JSON.stringify(playingCards)}
+    //             </>
+    //         )}
+    //     </>
+    // )
     
+}
+
+const MTGTable = (props) => {
+    return (
+        <> loading </>
+    )
 }
 
 export default PlayingCardsList
