22f691bfa61215ac66ef67dab87d24b397f5330b
Phil Brockman <phil.brockman@gmail.com>
2021-04-03 22:14:21 -0400

    games works

--- client/src/app/PlayGame.js
+++ client/src/app/PlayGame.js
@@ -38,8 +38,6 @@ export const PlayGame = props => {
   }
 
   const activateHordeDeck = event => {
-    //deep copy the current deck
-    // let currentDeck = cardsInZones[loxs.LIBRARY]
     let currentDeck = Utils.filterByLocation(hordeDeck, loxs.LIBRARY)
     let out = []
     let next
